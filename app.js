f8ec79e56410f44921acd88621c867c59e3c8550
Martins_dev <mtz.martinss03@gmail.com>
2025-04-20 23:22:33 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const session = require('express-session');
+const app = express();
+const authRoutes = require('./routes/authRoutes');
+const path = require('path');
+require('dotenv').config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Configuração de sessão
+app.use(session({
+  secret: 'mySecret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Rotas de autenticação
+app.use(authRoutes);
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
