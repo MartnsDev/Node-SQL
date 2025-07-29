475070b168b4d58b12ff36a78c92c5b52622c78c
Martins_dev <mtz.martinss03@gmail.com>
2025-07-29 05:03:18 -0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,11 @@
 const db = require('../config/db');
 const bcrypt = require('bcryptjs');
 
-// Função para encontrar um usuário pelo e-mail
 const findUserByEmail = (email, callback) => {
   const query = 'SELECT * FROM usuarios WHERE email = ?';
   db.query(query, [email], callback);
 };
 
-// Função para criar um novo usuário
 const createUser = (name, email, password, callback) => {
   const hashPassword = bcrypt.hashSync(password, 10);
   const query = 'INSERT INTO usuarios (nome, email, senha_hash) VALUES (?, ?, ?)';
