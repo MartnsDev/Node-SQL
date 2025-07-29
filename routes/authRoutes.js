c79c289bc5d9f06a61cf558e979bd2da1d39a15d
Martins_dev <mtz.martinss03@gmail.com>
2025-07-29 05:03:00 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,22 +2,16 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Página de login
 router.get('/login', authController.loginPage);
 
-// Ação de login
 router.post('/login', authController.loginAction);
 
-// Página de registro
 router.get('/register', authController.registerPage);
 
-// Ação de registro
 router.post('/register', authController.registerAction);
 
-// Página do dashboard (apenas para usuários autenticados)
 router.get('/dashboard', authController.dashboardPage);
 
-// Logout
 router.get('/logout', authController.logout);
 
 module.exports = router;
